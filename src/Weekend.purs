8e8673c62eb4cf8aafa203032755b6f9670f656c
Stefan Holzmüller <stefanholzmueller@github>
2017-10-23 20:56:30 +0200

    alternative implementation of itemized duration

--- src/Weekend.purs
+++ src/Weekend.purs
@@ -11,12 +11,12 @@ import Data.DateTime.Locale (Locale(..))
 import Data.Either (either)
 import Data.Enum (fromEnum, toEnum)
 import Data.Formatter.DateTime (formatDateTime)
-import Data.Formatter.Number (Formatter(..), format)
-import Data.Int (floor, toNumber)
-import Data.Maybe (Maybe(..), fromMaybe, isNothing)
-import Data.Time.Duration (class Duration, Days(..), Hours(..), Milliseconds(..), Minutes(..), Seconds(..), convertDuration)
+import Data.Int (fromNumber, toNumber)
+import Data.Maybe (Maybe(..), fromJust, fromMaybe, isNothing)
+import Data.Time.Duration (class Duration, Days(..), Milliseconds(..), Minutes)
 import Data.Tuple (Tuple(..))
-import Math (remainder)
+import Math (floor, remainder)
+import Partial.Unsafe (unsafePartial)
 
 
 weekendStartDayOfWeek :: Int
@@ -53,34 +53,42 @@ durationTillWeekend = map testWeekend currentLocalTime
         startDatetime = modifyTime ((setHour $ fromMaybe bottom $ toEnum weekendStartHour) >>> (setMinute $ fromMaybe bottom $ toEnum 0) >>> (setSecond $ fromMaybe bottom $ toEnum 0) >>> (setMillisecond $ fromMaybe bottom $ toEnum 0)) startDate
      in if startDatetime < now
         then Nothing
-        else let timeFormatter = Formatter { comma: false, before: 2, after: 0, abbreviations: false, sign: false }
-                 difference :: ItemizedDuration
+        else let difference :: ItemizedDuration
                  difference = diff startDatetime now
-              in case difference of
-                (Itemized (Days d) (Hours h) (Minutes m) (Seconds s) (Milliseconds ms)) ->
-                  let dd = show $ floor d
-                      hh = format timeFormatter h
-                      mm = format timeFormatter m
-                      ss = format timeFormatter s
-                  in Just $ (if d > 0.0 then dd <> " days and " else "") <> hh <> ":" <> mm <> ":" <> ss
+                 (Itemized itemized) = difference
+                 formatTime t = if t < 10 then "0" <> show t else show t
+                 dd = case itemized.days of
+                        0 -> ""
+                        1 -> "1 day and "
+                        d -> show d <> " days and "
+                 hh = formatTime itemized.hours
+                 mm = formatTime itemized.minutes
+                 ss = formatTime itemized.seconds
+              in Just $ dd <> hh <> ":" <> mm <> ":" <> ss
 
-msInSecond :: Number
-msInSecond = 1000.0
-msInMinute :: Number
-msInMinute = 60.0 * msInSecond
-msInHour :: Number
-msInHour = 60.0 * msInMinute
-msInDay :: Number
-msInDay = 24.0 * msInHour
 
-data ItemizedDuration = Itemized Days Hours Minutes Seconds Milliseconds
+data ItemizedDuration = Itemized { days :: Int
+                                 , hours :: Int
+                                 , minutes :: Int
+                                 , seconds :: Int
+                                 , ms :: Number
+                                 }
 
 instance durationItemized :: Duration ItemizedDuration where
-  fromDuration (Itemized (Days d) (Hours h) (Minutes m) (Seconds s) (Milliseconds ms)) =
-    Milliseconds (d * msInDay + h * msInHour + m * msInMinute + s * msInSecond + ms)
-  toDuration (Milliseconds ms') = Itemized (Days d) (Hours h) (Minutes m) (Seconds s) (Milliseconds ms)
-    where divMod x y = Tuple (div x y) (remainder x y)
+  fromDuration (Itemized { days, hours, minutes, seconds, ms }) =
+    Milliseconds (toNumber (days * msInDay + hours * msInHour + minutes * msInMinute + seconds * msInSecond) + ms)
+    where msInSecond = 1000
+          msInMinute = 60 * msInSecond
+          msInHour = 60 * msInMinute
+          msInDay = 24 * msInHour
+  toDuration (Milliseconds ms') = Itemized { days, hours, minutes, seconds, ms }
+    where toInt n = unsafePartial $ fromJust $ fromNumber n
+          divMod x y = Tuple (floor $ div x y) (remainder x y)
           Tuple s' ms = divMod ms' 1000.0
           Tuple m' s  = divMod s' 60.0
           Tuple h' m  = divMod m' 60.0
           Tuple d  h  = divMod h' 24.0
+          days    = toInt d  -- Number.MAX_SAFE_INTEGER / 86400000 is smaller than 2^31
+          hours   = toInt h
+          minutes = toInt m
+          seconds = toInt s
