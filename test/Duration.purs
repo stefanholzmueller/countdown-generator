193749061bd04367cbdd3c8901b5ea44b2fc131e
Stefan Holzmüller <stefanholzmueller@github>
2017-10-28 14:29:55 +0200

    test for correct ranges

--- test/Duration.purs
+++ test/Duration.purs
@@ -1,16 +1,18 @@
 module Test.Duration where
 
 import Prelude
+
 import Data.Time.Duration (Milliseconds(..), convertDuration)
---import Debug.Trace (spy)
-import Test.StrongCheck (class Arbitrary, Result, SC, assertEq, quickCheck)
+import Data.Ord (abs)
+import Test.StrongCheck (class Arbitrary, Result, SC, assertEq, quickCheck, (<?>))
 import Test.StrongCheck.Gen (choose)
-import Weekend (ItemizedDuration)
+import Weekend (ItemizedDuration(..))
 
 
 durationTests :: forall eff. SC eff Unit
 durationTests = do
   quickCheck convertToItemizedDurationAndBack
+  quickCheck itemizedDurationHasCorrectRanges
 
 convertToItemizedDurationAndBack :: RandomMilliseconds -> Result
 convertToItemizedDurationAndBack (RandomMilliseconds milliseconds) =
@@ -21,10 +23,20 @@ convertToItemizedDurationAndBack (RandomMilliseconds milliseconds) =
   returnedMilliseconds :: Milliseconds
   returnedMilliseconds = convertDuration itemizedDuration
 
+itemizedDurationHasCorrectRanges :: RandomMilliseconds -> Result
+itemizedDurationHasCorrectRanges (RandomMilliseconds milliseconds) =
+  (((abs itemized.ms) < 1000.0) <?> ("incorrect ms: " <> show itemized.ms)) <>
+  (((abs itemized.seconds) < 60) <?> ("incorrect seconds: " <> show itemized.seconds)) <>
+  (((abs itemized.minutes) < 60) <?> ("incorrect minutes: " <> show itemized.minutes)) <>
+  (((abs itemized.hours) < 24) <?> ("incorrect hours: " <> show itemized.hours))
+  where
+  itemizedDuration :: ItemizedDuration
+  itemizedDuration = convertDuration milliseconds
+  (Itemized itemized) = itemizedDuration
+
 newtype RandomMilliseconds = RandomMilliseconds Milliseconds
 instance arbMilliseconds :: Arbitrary RandomMilliseconds
   where
   arbitrary = do
     number <- choose (-9007199254740991.0) 9007199254740991.0
     pure (RandomMilliseconds $ Milliseconds number)
-
