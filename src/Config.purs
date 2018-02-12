3bc80128f3db752f14df7b5d33431dbc4a9779c4
Stefan Holzmüller <stefanholzmueller@github>
2018-02-12 15:30:40 +0100

    serialize config and prepare deserialization

--- src/Config.purs
+++ src/Config.purs
@@ -2,17 +2,36 @@ module Config where
   
 import Prelude
 
-import Data.Date (Date, Month(..), canonicalDate)
-import Data.Enum (toEnum)
+import Data.Date (Date, Month(..), canonicalDate, day, month, year)
+import Data.Either (Either(..))
+import Data.Enum (fromEnum, toEnum)
 import Data.Maybe (Maybe(..), fromJust)
-import Data.Time (Time(..))
+import Data.Time (Time(..), hour, minute, second)
+import Data.URI (Query(..), URI(..))
 import Partial.Unsafe (unsafePartial)
 
 
 data Config = Weekly { startDayOfWeek :: Int, startTime :: Time }
             | Fixed { startDate :: Date, startTime :: Time }
 
+instance showConfig :: Show Config -- for debugging
+  where show config = serialize config
+
 config :: Config
 config = Fixed { startDate: unsafePartial $ fromJust $ canonicalDate <$> toEnum 2017 <*> (Just November) <*> toEnum 17
                , startTime: unsafePartial $ fromJust $ Time <$> toEnum 17 <*> toEnum 0 <*> toEnum 0 <*> toEnum 0
                }
+
+serialize :: Config -> String
+serialize config = case config of
+  (Weekly { startDayOfWeek, startTime }) -> "?type=weekly&startDayOfWeek=" <> show startDayOfWeek <> timeParam startTime
+  (Fixed { startDate, startTime }) -> "?type=fixed" <> dateParam startDate <> timeParam startTime
+  where
+  dateParam :: Date -> String
+  dateParam date = "&year=" <> (show <<< fromEnum <<< year) date <> "&month=" <> (show <<< fromEnum <<< month) date <> "&day=" <> (show <<< fromEnum <<< day) date
+  timeParam :: Time -> String
+  timeParam time = "&hour=" <> (show <<< fromEnum <<< hour) time <> "&minute=" <> (show <<< fromEnum <<< minute) time <> "&second=" <> (show <<< fromEnum <<< second) time
+
+deserialize :: URI -> Either String Config
+deserialize (URI _ _ (Just (Query params)) _) = Right config
+deserialize _ = Left "URI has no query parameters"
--- src/Main.purs
+++ src/Main.purs
@@ -3,13 +3,15 @@ module Main where
 import Prelude
 
 import Component (Query(..), component)
+import Config as C
 import Control.Monad.Aff.Console (CONSOLE, log)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Now (NOW)
 import DOM.HTML (window)
-import DOM.HTML.Location (search)
+import DOM.HTML.Location (href)
 import DOM.HTML.Window (location)
-import Data.URI.Query (parser)
+import Data.Either (Either(..))
+import Data.URI.URI (parser)
 import Halogen as H
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
@@ -18,11 +20,15 @@ import Text.Parsing.StringParser (runParser)
 
 main :: forall eff. Eff (HA.HalogenEffects (console :: CONSOLE, now :: NOW | eff)) Unit
 main = HA.runHalogenAff do
-  w <- H.liftEff window
-  l <- H.liftEff $ location w
-  s <- H.liftEff $ search l
-  log s
-  log $ show $ runParser parser s
+  h <- H.liftEff $ window >>= location >>= href
+  let config = loadConfig h
+  log $ show $ config
   body <- HA.awaitBody
   io <- runUI component unit body
   H.liftAff $ io.query $ H.action Tick
+  where
+  loadConfig :: String -> Either String C.Config
+  loadConfig location = case runParser parser location of
+    (Left parseError) -> Left $ show parseError
+    (Right uri) -> C.deserialize uri
+  
